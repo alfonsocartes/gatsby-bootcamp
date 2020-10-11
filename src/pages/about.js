b72d7605e8e0be151a27740cbb4a6b357513b755
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2020-10-11 11:59:00 +0100

    Add basic pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I'm Alfonso, a full-stack developer traveling across the UK.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>The best way to reach me is via @cartes_dev on Twitter!</p>
+    </div>
+  )
+}
+
+export default ContactPage
