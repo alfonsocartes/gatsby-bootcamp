0e7ac156d2338a66be54ffd23b4f5f5098ef70d7
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2020-10-12 09:09:57 +0100

    Remove contact link from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Alfonso, a full-stack developer traveling across the UK.</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me.</Link>
-      </p>
     </Layout>
   )
 }
