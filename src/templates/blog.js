02f4d0dcc352064ec7eac9b06bd79653c9e26e85
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2020-10-12 08:34:03 +0100

    Add images to blog posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -17,11 +17,23 @@ export const query = graphql`
 `
 
 const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
+        return <img alt={alt} src={url} />
+      },
+    },
+  }
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
-      {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
     </Layout>
   )
 }
